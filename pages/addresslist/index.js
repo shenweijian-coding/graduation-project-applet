2600b9a8bcc20e67ccf661fea65d8ef962b12d38
shenweijian <1834638245@qq.com>
2021-03-11 20:44:17 +0800

    社团、通讯录静态UI

--- pages/addresslist/index.js
+++ pages/addresslist/index.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    addressType:[
+      {
+        text: ''
+      }
+    ]
   },
 
   /**
--- pages/club/index.js
+++ pages/club/index.js
@@ -7,7 +7,12 @@ Page({
   data: {
 
   },
-
+  onChange(event) {
+    wx.showToast({
+      title: `切换到标签 ${event.detail.name}`,
+      icon: 'none',
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
